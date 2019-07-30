06c3cf7a54f6f608d2740aa70aee5b9d942ac4f3
Ana Djurkovic <ana_djurkovic@hotmail.com>
2019-07-30 04:49:18 -0400

    Add files via upload

---
+++ mainGame.py
@@ -0,0 +1,41 @@
+import pygame
+
+pygame.init()
+
+#800 pixels wide, 480 pixels tall
+gameDisplay = pygame.display.set_mode((800,480)) #use tuple (()) or list([])
+pygame.display.set_caption('Emotions Game')
+
+
+#**************** MAIN GAMEPLAY *********************************************
+
+#MAIN TITLE SCREEN ANIMATION + CLICKABLE PLAY BUTTON
+
+#MAKI EXPLAINS HOW TO PLAY (AUDIO + VISUAL ON SCREEN)
+# (We need to write dialogue for what maki will say for each part of the game
+#  then do a text-to-speech recording or something)
+
+#SCREEN SAYS TO WATCH CLOSELY, ONCE SCREEN IS TOUCHED TRIGGER A VIDEO TO PLAY
+
+#WHEN VIDEO IS DONE, SHOW CLICKABLE EMOTION OPTIONS
+
+#WHEN EMOTION IS CHOSEN, STORE DATA (CORRECT OR NOT ETC), SHOW SOMETHING FUN
+#OR EDUCATIONAL ON SCREEN (prof emailed about making the game about learning
+# and not just testing so we need to squeeze in learning between testing
+
+#***************************************************************************
+
+
+
+#***************   SETTINGS  ************************************************
+
+# (refer to flowchart on google drive for a rough plan for settings)
+
+
+
+
+#****************************************************************************
+
+
+
+
