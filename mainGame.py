2e267897c24027bd8e1cb8f0ed057c2a2f022d2a
Ana Djurkovic <ana_djurkovic@hotmail.com>
2019-07-30 04:48:16 -0400

    Delete mainGame.py

--- mainGame.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-pygame.init()
-
-#800 pixels wide, 480 pixels tall
-gameDisplay = pygame.display.set_mode((800,480)) #use tuple (()) or list([])
-pygame.display.set_caption('Emotions Game')
